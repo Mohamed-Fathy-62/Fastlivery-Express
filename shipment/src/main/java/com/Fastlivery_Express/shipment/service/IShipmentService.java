f7ffc655d8d70faa8287c959caf6f977f5776ffd
Mohamed Fathy <mofathy622000@gmail.com>
2026-03-09 00:25:57 +0200

    add shipment service(draft work)

---
+++ shipment/src/main/java/com/Fastlivery_Express/shipment/service/IShipmentService.java
@@ -0,0 +1,10 @@
+package com.Fastlivery_Express.shipment.service;
+
+import com.Fastlivery_Express.shipment.dto.ShipmentDto;
+
+public interface IShipmentService {
+    ShipmentDto createShipment(ShipmentDto shipmentDto);
+//    ShipmentDto getUserById(Long id);
+//    boolean updateUser(Long id, ShipmentDto userDto);
+//    boolean deleteUser(Long id);
+}
