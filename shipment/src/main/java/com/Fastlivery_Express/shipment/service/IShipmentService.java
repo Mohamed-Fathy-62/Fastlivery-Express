0a14ef9139c4e9659f52cdb89aff65b20a4ce95e
Mohamed Fathy <mofathy622000@gmail.com>
2026-03-09 21:49:17 +0200

    complete simple CRUD Shipment Service v0

--- shipment/src/main/java/com/Fastlivery_Express/shipment/service/IShipmentService.java
+++ shipment/src/main/java/com/Fastlivery_Express/shipment/service/IShipmentService.java
@@ -4,7 +4,7 @@ import com.Fastlivery_Express.shipment.dto.ShipmentDto;
 
 public interface IShipmentService {
     ShipmentDto createShipment(ShipmentDto shipmentDto);
-//    ShipmentDto getUserById(Long id);
-//    boolean updateUser(Long id, ShipmentDto userDto);
-//    boolean deleteUser(Long id);
+    ShipmentDto getShipmentById(Long id);
+    boolean updateShipment(Long id, ShipmentDto userDto);
+    boolean deleteShipment(Long id);
 }
