8950b97bdd2f4a6aa7c0168b6dc2d5f1c0cc6c22
Mohamed Fathy <mofathy622000@gmail.com>
2026-03-12 00:24:44 +0200

    add config server

---
+++ configserver/src/test/java/com/Fastlivery_Express/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fastlivery_Express.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
